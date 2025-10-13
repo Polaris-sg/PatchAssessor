146d8e965b8167eb9abfcf854fe231fc62b27f51
Zhan Shanggui <104077220+Polaris-sg@users.noreply.github.com>
2025-10-13 17:11:49 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -31,64 +31,6 @@ class StarCoderTokenizer:
         return self.tokenizer.decode(token_ids, skip_special_tokens=True)
 
 
-class CONSTANTS:
-    max_hop = 5
-    max_search_top_k = 10
-    max_statement = 20
-    repo_base_dir = f"./repositories"
-    graph_database_save_dir = f"./context_database"
-    query_graph_save_dir = "./graph_based_query"
-    dataset_dir = "./RepoEval-Updated"
-    
-    # Tree-sitter language libraries path
-    TS_LANGUAGES_PATH = "./my-languages.so"  # Path to compiled tree-sitter languages
-    
-    repos = ["devchat",
-             "nemo_aligner",
-             "awslabs_fortuna",
-             "task_weaver",
-             "huggingface_diffusers",
-             "opendilab_ACE",
-             "metagpt",
-             "apple_axlearn",
-             "QingruZhang_AdaLoRA",
-             "nerfstudio-project_nerfstudio",
-             "itlemon_chatgpt4j",
-             "Aelysium-Group_rusty-connector",
-             "neoforged_NeoGradle",
-             "mybatis-flex_mybatis-flex",
-             "Guiqu1aixi_rocketmq",
-             "SimonHalvdansson_Harmonic-HN",
-             "Open-DBT_open-dbt",
-             "QuasiStellar_custom-pixel-dungeon",
-             "gentics_cms-oss",
-             "FloatingPoint-MC_MIN"
-             ]
-
-    repos_language = {
-         "devchat": "python",
-         "nemo_aligner": "python",
-         "awslabs_fortuna": "python",
-         "task_weaver": "python",
-         "huggingface_diffusers": "python",
-         "opendilab_ACE": "python",
-         "metagpt": "python",
-         "apple_axlearn": "python",
-         "QingruZhang_AdaLoRA": "python",
-         "nerfstudio-project_nerfstudio": "python",
-         "itlemon_chatgpt4j": "java",
-         "Aelysium-Group_rusty-connector": "java",
-         "neoforged_NeoGradle": "java",
-         "mybatis-flex_mybatis-flex": "java",
-         "Guiqu1aixi_rocketmq": "java",
-         "SimonHalvdansson_Harmonic-HN": "java",
-         "Open-DBT_open-dbt": "java",
-         "QuasiStellar_custom-pixel-dungeon": "java",
-         "gentics_cms-oss": "java",
-         "FloatingPoint-MC_MIN": "java"
-    }
-
-
 class CodexTokenizer:
     def __init__(self):
         self.tokenizer = tiktoken.encoding_for_model("gpt-3.5-turbo-instruct")
